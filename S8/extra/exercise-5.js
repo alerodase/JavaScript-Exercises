8be0d55ebc9ac0214064c072a1bbb8dd0582ade4
alerodase <alerodase@gmail.com>
2024-03-26 20:41:47 +0100

    practicando

--- S8/extra/exercise-5.js
+++ S8/extra/exercise-5.js
@@ -9,11 +9,11 @@ const inputQuestionNumber=document.querySelector(`input[data-function="questions
 const btnStartGame= document.querySelector(`button[data-function="start-game"]`)
 const divGameBoard=document.querySelector(`div[data-function="gameboard"]`)
 const btnCheckGame= document.querySelector(`button[data-function="check-game"]`)
+let correctAnswers=0
 
 
 btnStartGame.addEventListener("click", async ()=>{
-
-    const api= await fetch(`https://opentdb.com/api.php?amount=${inputQuestionNumber.value}&type=multiple`)
+    const api= await fetch(`https://opentdb.com/api.php?amount=${inputQuestionNumber.value}`)
     const res= await api.json();
 
     if(inputQuestionNumber.value!=="" && inputQuestionNumber.value!==0){
@@ -24,10 +24,8 @@ btnStartGame.addEventListener("click", async ()=>{
         divGameBoard.appendChild(questionDiv)
 
         const questionH3=document.createElement("h3")
-        const ulAnswer=document.createElement("ul")
         
         questionDiv.appendChild(questionH3)
-        questionDiv.appendChild(ulAnswer)
 
         
         questionH3.innerHTML=i+1+". "+res.results[i].question
@@ -36,20 +34,44 @@ btnStartGame.addEventListener("click", async ()=>{
         const allAnswers = [...res.results[i].incorrect_answers, res.results[i].correct_answer];        
         allAnswers.sort(()=>Math.random()-0.5)
 
-        for (let j = 0; j < allAnswers.length; j++) {
-           const answers=document.createElement("li")
-           answers.innerHTML=allAnswers[j]
-           ulAnswer.appendChild(answers) 
-        }
-
-        const inputAnswer=document.createElement(`input`)
-        inputAnswer.setAttribute("placeholder", "Escribe tu respuesta")
-        questionDiv.appendChild(inputAnswer)
+        const selectAnswer=document.createElement(`select`)
+        selectAnswer.setAttribute("style", "width: 200px")
+        const option1= document.createElement("option")
+        const option2= document.createElement("option")
+        const option3= document.createElement("option")
+        const option4= document.createElement("option")
+        questionDiv.appendChild(selectAnswer)
+        selectAnswer.appendChild(option1)
+        selectAnswer.appendChild(option2)
+        option1.innerHTML=allAnswers[0]
+        option1.value=allAnswers[0]
+        option2.innerHTML=allAnswers[1]
+        option2.value=allAnswers[1]
+        if(allAnswers.length>2){
+        selectAnswer.appendChild(option3)
+        selectAnswer.appendChild(option4)
+        option3.innerHTML=allAnswers[2]
+        option3.value=allAnswers[2]
+        option4.innerHTML=allAnswers[3]
+        option4.value=allAnswers[3]}
         console.log(allAnswers);
-    }
+        const h2Correct=document.createElement("h2")
+        questionDiv.appendChild(h2Correct)
+        h2Correct.innerHTML=""
+        
+        btnCheckGame.addEventListener("click", ()=>{
 
-    }
-    
-})
+        
+            if(selectAnswer.value===res.results[i].correct_answer){
+                h2Correct.innerHTML="¡¡CORRECT!!"
+                h2Correct.setAttribute("style","font-weight: bold; color: green")
+                correctAnswers++;
+            }else{
+                h2Correct.innerHTML="ERROR"
+                h2Correct.setAttribute("style","font-weight: bold; color: red")
+            }
+            btnCheckGame.disabled=true})
+   }}
+btnStartGame.disabled=true })
 
 
